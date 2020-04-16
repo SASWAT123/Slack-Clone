60471df268a02039d52355666c2b67c58a56d3b2
SASWAT123 <psaswat21@gmail.com>
2020-04-17 01:51:24 +0530

    creating channels and storing in global store

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 import firebase from "../../firebase";
-import { connect } from "react-redux";
 
 class UserPanel extends React.Component {
   state = {
     user: this.props.currentUser,
   };
-
-  componentDidMount() {
-    this.setState({
-      user: this.props.currentUser,
-    });
-  }
 
   dropdownOptions = () => [
     {
@@ -30,34 +23,36 @@ class UserPanel extends React.Component {
     },
     {
       key: "signout",
-      text: <span onClick={this.handleSignOut}>Sign Out</span>,
+      text: <span onClick={this.handleSignout}>Sign Out</span>,
     },
   ];
 
-  handleSignOut = () => {
+  handleSignout = () => {
     firebase
       .auth()
       .signOut()
-      .then(() => console.log("User signed out!"));
+      .then(() => console.log("signed out!"));
   };
 
   render() {
     const { user } = this.state;
+
     return (
       <Grid style={{ background: "#4c3c4c" }}>
         <Grid.Column>
           <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
-            {/* App header */}
+            {/* App Header */}
             <Header inverted floated="left" as="h2">
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
-            {/* User Dropdown */}
+
+            {/* User Dropdown  */}
             <Header style={{ padding: "0.25em" }} as="h4" inverted>
               <Dropdown
                 trigger={
                   <span>
-                    <Image src={user.photoUrl} spaced="right" avatar />
+                    <Image src={user.photoURL} spaced="right" avatar />
                     {user.displayName}
                   </span>
                 }
@@ -71,8 +66,4 @@ class UserPanel extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
-});
-
-export default connect(mapStateToProps)(UserPanel);
+export default UserPanel;
