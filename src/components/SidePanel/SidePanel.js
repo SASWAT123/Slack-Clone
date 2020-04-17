7fa04d611375fb467f8c3ae670132a7d2a8cd215
SASWAT123 <psaswat21@gmail.com>
2020-04-17 20:46:51 +0530

    create and display message

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -5,7 +5,6 @@ import Channel from "./Channels";
 
 class SidePanel extends React.Component {
   render() {
-    const { currentUser } = this.props;
     return (
       <Menu
         size="large"
@@ -14,8 +13,8 @@ class SidePanel extends React.Component {
         vertical
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
-        <UserPanel currentUser={currentUser} />
-        <Channel currentUser={currentUser} />
+        <UserPanel currentUser={this.props.currentUser} />
+        <Channel currentUser={this.props.currentUser} />
       </Menu>
     );
   }
